edd2ebebc284e000526d9ebc5295a234ffb2e574
Alexey Naborshchikov <to.magus@gmail.com>
2025-10-30 15:25:15 +0100

    containerization

--- src/test/java/natera/task/local/url_shortener/UrlShortenerApplicationTests.java
+++ src/test/java/natera/task/local/url_shortener/UrlShortenerApplicationTests.java
@@ -2,8 +2,10 @@ package natera.task.local.url_shortener;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class UrlShortenerApplicationTests {
 
 	@Test
